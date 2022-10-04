4bf99825476745b651425f6b37b4ef542cf11c79
Daniel Gutierrez <gutierrezrochadanielalejandro@gmail.com>
2022-10-04 09:41:16 -0600

    se agrego de nuevo el archivo constantes.cpp

---
+++ constantes.cpp
@@ -0,0 +1,16 @@
+/*
+Nombre del archivo : sumanumeros.cpp
+Autor : Daniel Gutierrez
+lugar : ITV
+instrucciones : esto solo es una plantilla
+*/
+#include<iostream>
+
+usig namespace std;
+
+int main(){
+	
+	return 0;
+}
+
+
